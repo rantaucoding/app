99fb56cd141b69bb504de996a0d76c8aaf86a0e6
rantaucoding <117258026+rantaucoding@users.noreply.github.com>
2022-11-02 11:15:17 +0000

    Deploying to gh-pages from @ rantaucoding/app@bb2408dce77ceb324f2433749bf2f1d16f3baf1c 🚀

---
+++ precache-manifest.60ac82251a7fd84071df293a6405547a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "67dc6914790568c5198b",
+    "url": "/app/static/css/main.2c23ad0e.chunk.css"
+  },
+  {
+    "revision": "67dc6914790568c5198b",
+    "url": "/app/static/js/main.c5ca16b9.chunk.js"
+  },
+  {
+    "revision": "aef6ff46aaadf91cc8a2",
+    "url": "/app/static/js/runtime~main.33e944a4.js"
+  },
+  {
+    "revision": "ca8562f04f27eeb964b0",
+    "url": "/app/static/js/2.a77e7803.chunk.js"
+  },
+  {
+    "revision": "e2a1c4f2c2867a5e5676",
+    "url": "/app/static/js/3.a853dc92.chunk.js"
+  },
+  {
+    "revision": "5ff112a1ea6266fc5399be27af75bfaf",
+    "url": "/app/index.html"
+  }
+];
